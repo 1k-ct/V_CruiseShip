898d6626b484e5ecf5daf273120bd3acd6037691
1k-ct <268iop@gmail.com>
2020-08-10 23:17:38 +0900

    user controller

---
+++ controller/user_controller.go
@@ -0,0 +1,48 @@
+package controller
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+
+	glv "github.com/1k-ct/V_CruiseShip/getlivevideo"
+)
+
+// Controller is user controlller
+type Controller struct{}
+
+//動画の情報を取ってくるURL
+var url string = "https://virtual-youtuber.userlocal.jp/lives"
+
+func startCruise(url string) ([]string, bool) {
+	dataLink := glv.GetLivingVideo(url) //動画をスクレイピングしてくる
+	log.Println("スクレイピング出来たよ！")
+
+	return dataLink, true
+}
+
+//Home 一番初めのページ "/ggnew"
+func (pc Controller) Home(c *gin.Context) {
+	c.HTML(http.StatusOK, "start.html", gin.H{})
+}
+
+//VideoStart 視聴スタート
+func (pc Controller) VideoStart(c *gin.Context) {
+	dataLink, _ := startCruise(url)
+
+	for _, v := range dataLink {
+		c.HTML(http.StatusOK, "index.html", gin.H{"dataLink": v})
+	}
+	c.Redirect(302, "/ggnew")
+}
+
+//Interim VideoStart が、最後まで行われるとInterimが実行される
+func (pc Controller) Interim(c *gin.Context) {
+	c.Redirect(302, "/new")
+}
+
+// Stop 緊急時のストップポイント
+func (pc Controller) Stop(c *gin.Context) {
+	c.HTML(http.StatusOK, "stoppoint.html", gin.H{})
+}
