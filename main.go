9ca1988aa52b64ef4a9c9f3a7ffaa0ecf4791ff9
1k-ct <268iop@gmail.com>
2020-08-11 16:48:06 +0900

    masterのserver出来てない

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
-import server "github.com/1k-ct/V_CruiseShip/server"
+import server "./server"
+
+//import server "github.com/1k-ct/V_CruiseShip/server"
 
 func main() {
 	server.Init()
