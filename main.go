09fb3b00d326a7706304bad3678ec1ab198f4676
1k-ct <59008019+1k-ct@users.noreply.github.com>
2020-08-20 15:05:27 +0900

    Update main.go
    main

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
-import server "./server"
-
-//import server "github.com/1k-ct/V_CruiseShip/server"
+import server "github.com/1k-ct/V_CruiseShip/server"
 
 func main() {
 	server.Init()
